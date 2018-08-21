24eb445b26efdf5ac9541eb8a0cfe357af8f1a0d
Zhou <stone.zhou@emerson.com>
2018-08-21 09:15:52 +0800

    08212018

---
+++ Csharp/Interface/Interface.cs
@@ -0,0 +1,39 @@
+using System;
+
+interface IMyInterface
+{
+    // 接口成员
+    void MethodToImplement();
+	void MethodToImplement1();
+	void MethodToImplement2();
+}
+
+class InterfaceImplementer : IMyInterface
+{
+    static void Main()
+    {
+		NewMethod();
+        InterfaceImplementer iImp = new InterfaceImplementer();
+        iImp.MethodToImplement();
+		iImp.MethodToImplement2();
+		iImp.MethodToImplement1();
+    }
+
+    public void MethodToImplement()
+    {
+        Console.WriteLine("MethodToImplement() called.");
+    }
+	    public void MethodToImplement1()
+    {
+        Console.WriteLine("MethodToImplement() called.");
+    }
+	    public void MethodToImplement2()
+    {
+        Console.WriteLine("MethodToImplement() called.");
+    }
+	
+	    private static void NewMethod()
+        {
+            Console.WriteLine("Hello Worl1d!");
+        }
+}
