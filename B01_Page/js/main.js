73c81c4551414d1f03ac0d5d7645c055706a631c
Zhou <stone.zhou@emerson.com>
2019-07-10 14:51:10 +0800

    20190710dell

--- B01_Page/js/main.js
+++ B01_Page/js/main.js
@@ -1,20 +1,28 @@
+var newItemCouter = 1;
 var ourList = document.getElementById("our-list");
 var ourButton = document.getElementById("our-button");
 var ourHeadline = document.getElementById("our-headLine");
 var listItems = document.getElementById("our-list").getElementsByTagName("li");
 
-for(i=0;i<listItems.length; i++){
-   // listItems[i].innerHTML="HELLO WORLD";
-    listItems[i].addEventListener("click",activateItem);
+for (i = 0; i < listItems.length; i++) {
+    // listItems[i].innerHTML="HELLO WORLD";
+    listItems[i].addEventListener("click", activateItem);
 
 }
 
-function activateItem(){
+function activateItem() {
     //alert("click detected");
-    ourHeadline.innerHTML = this.innerHTML;
+    ourHeadline.innerHTML = this.innerHTML; //update head line content
+
+    for (i = 0; i < listItems.length; i++) {
+        // listItems[i].innerHTML="HELLO WORLD";
+        listItems[i].classList.remove("active");  //remove color
+    }
+    this.classList.add("active"); //add color
 }
 
-ourButton.addEventListener("click",createNewitem);
-function createNewitem(){
-ourList.innerHTML += "<li>something new</li>";
+ourButton.addEventListener("click", createNewitem);
+function createNewitem() {
+    ourList.innerHTML += "<li>something new" + newItemCouter + "</li>";
+    newItemCouter++;
 }
