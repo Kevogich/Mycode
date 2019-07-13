a60ab748bffb86d0894fefaef8cbb3cf8ffafbd2
ZhouQY <zqy104@163.com>
2019-07-13 08:20:01 +0800

    create new

---
+++ P01_guldeSizingTool/Rebuild5100Bom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rebuild5100Bom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
